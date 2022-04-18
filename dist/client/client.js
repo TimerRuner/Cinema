27598e95f26ede88a6bd6829e1101c9ba62f94cd
practice-stack1 <vadim3d2011@gmail.com>
2022-04-18 18:32:50 +0300

    heroku deploy

--- dist/client/client.js
+++ dist/client/client.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29dea04bea7fb5672711";
+/******/ 	var hotCurrentHash = "da82192481e01e589047";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
